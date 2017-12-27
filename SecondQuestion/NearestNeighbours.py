68c682584b446daaf1bd80008001bed85dceb892
cthem <c.themeli@gnt.gr>
2017-12-27 17:45:39 +0200

    update report

--- SecondQuestion/NearestNeighbours.py
+++ SecondQuestion/NearestNeighbours.py
@@ -1,5 +1,4 @@
 import FirstQuestion.CleanData as clean
-import datetime
 import os
 import utils
 
@@ -17,9 +16,16 @@ def question_a1(output_folder, file, trips_list):
 
 
 def find_nearest_neighbours_for_test_trip(test_trip, trips_list):
+
+    '''
+    :param test_trip: a give trip from the test file
+    :param trips_list: all trips from the training_set file
+    :return: returns the 5 nearest neighbours for the given test trip
+    '''
+
     test_lonlat = utils.create_list_tuples(test_trip)
     nearest_neighbours = []
-    #print("knn for ",test_lonlat)
+    # print("knn for ",test_lonlat)
     for trip in trips_list:
         trip_lonlat = utils.create_list_tuples(trip)
         distance = calculate_dynamic_time_warping(test_lonlat, trip_lonlat)
@@ -30,6 +36,12 @@ def find_nearest_neighbours_for_test_trip(test_trip, trips_list):
 
 
 def calculate_dynamic_time_warping(latlons1, latlons2):
+    '''
+    Calculate the DTW of two points, used in order find the nearest neighbours
+    :param latlons1:
+    :param latlons2:
+    :return:
+    '''
     dtw = [ [float('Inf') for _ in range(len(latlons2)+1)] for _ in range(len(latlons1)+1)]
     dtw[0][0] = 0
     for i in range(1,1+len(latlons1)):
@@ -40,13 +52,11 @@ def calculate_dynamic_time_warping(latlons1, latlons2):
 
 
 def get_updated_trips_list(trips_list, nearest_neighbours):
-    """
-    gets a trips_list as input, in the format of tripsClean.csv file and returns a list of tuples with lats and lons
-
-    :param trips_list:
-    :param nearest_neighbours:
-    :return:
-    """
+    '''
+    :param trips_list: all the trips from the training set
+    :param nearest_neighbours: the 5 nearest neighbours
+    :return: trips_list containing only the 5 nearest neighbours
+    '''
     new_trips_list=[]
     for trip in trips_list:
         for neighbour in nearest_neighbours:
@@ -56,6 +66,15 @@ def get_updated_trips_list(trips_list, nearest_neighbours):
 
 
 def preprocessing_for_visualization(test_trip, nearest_neighbours, trips_list, outfile_name, elapsed, i):
+    '''
+    :param test_trip: the given test trip from the test file
+    :param nearest_neighbours: the 5 nearest neighbours of this test trip
+    :param trips_list: all the trips from the training set file
+    :param outfile_name: name for the output file
+    :param elapsed: time needed in order to process the nearest neighbours
+    :param i: the index of the test trip
+    :return: calls the visualize_paths function in order to export the maps with the test trip and the 5 nearest neighbours
+    '''
     points = [[utils.idx_to_lonlat(list(range(len(test_trip[2:]))), test_trip)]]
     labels = ["test trip: %d" % i]
     colors = [['b'] for _ in range(len(nearest_neighbours) + 1)]
