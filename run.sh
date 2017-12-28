29dc8ffdae0a7dada87637aab55aebc986b4774b
cthem <c.themeli@gnt.gr>
2017-12-28 13:40:15 +0200

    small changes to report and add maps folder for questions 2a1 kai 2a2

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 [ -z $(which phantomjs) ] && echo "Need phantomjs to create output report files!" && exit 
-python Main.py "$(pwd)/results_folder" ./input_folder/train_set_dev.csv
+python Main.py "$(pwd)/input_folder" "$(pwd)/results_folder"
